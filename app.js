efa8812de0462b3060df44a8812d7d60930696df
aFish <tako0903@icloud.com>
2019-02-14 19:33:40 +0800

    random image func

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-
+const request = require('request');
 const linebot = require('linebot');
 const bot = linebot({
   channelId: process.env.PORT,
@@ -7,17 +7,26 @@ const bot = linebot({
   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
 });
 
+let imgURL = ''
+let getImage = function() {
+  request('http://35.194.134.133:5000/', function (error, response, body) {
+    var jsonObject = JSON.parse(body)
+    imgURL = jsonObject.url
+  })
+}
 // handle message
 bot.on('message', function (event) {
-	recv_msg = event.message.text
-	console.log('recv message:', recv_msg)
-	replay_msg = 'im a bot, your message is:\n' + recv_msg
+	let = recv_msg = event.message.text
+	console.log('message:', recv_msg)
   //replay
-  event.reply(replay_msg).then(function (data) {
-
-  }).catch(function (error) {
-
-  });
+  if (recv_msg=='抽') {
+    getImage()
+    event.reply({
+      type: 'image',
+      originalContentUrl: imgURL,
+      previewImageUrl: imgURL
+    });
+  }
 });
 
 bot.listen('/hook', 3000, function () {
