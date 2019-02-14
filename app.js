e9f31e2bcee97c295a1f4297cca479a08f083c62
aFish <tako0903@icloud.com>
2019-02-14 19:39:38 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const request = require('request');
+
 const linebot = require('linebot');
 const bot = linebot({
   channelId: process.env.PORT,
@@ -7,26 +7,17 @@ const bot = linebot({
   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
 });
 
-let imgURL = ''
-let getImage = function() {
-  request('http://35.194.134.133:5000/', function (error, response, body) {
-    var jsonObject = JSON.parse(body)
-    imgURL = jsonObject.url
-  })
-}
 // handle message
 bot.on('message', function (event) {
-	let = recv_msg = event.message.text
-	console.log('message:', recv_msg)
+	recv_msg = event.message.text
+	console.log('recv message:', recv_msg)
+	replay_msg = 'im a bot, your message is:\n' + recv_msg
   //replay
-  if (recv_msg=='抽') {
-    getImage()
-    event.reply({
-      type: 'image',
-      originalContentUrl: imgURL,
-      previewImageUrl: imgURL
-    });
-  }
+  event.reply(replay_msg).then(function (data) {
+
+  }).catch(function (error) {
+
+  });
 });
 
 bot.listen('/hook', 3000, function () {
